5dfa6f9bf69af17cb4c621d6901326ae9086f059
z.kontra <z.kontra@mail.ru>
2020-03-03 17:52:11 +0600

    Add project files.

---
+++ Calculator/Calculator.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calculator.aspx.cs" Inherits="Calculator.Calculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="margin-right: auto; margin-left: auto">
+            <asp:TextBox ID="tbx_expression" runat="server" Style="width: 200px"></asp:TextBox>
+            <asp:Button ID="Number_0_btn" runat="server" Text="0" OnClick="Number_0_btn_Click" />
+            <asp:Button ID="Number_1_btn" runat="server" Text="1" OnClick="Number_1_btn_Click" />
+            <asp:Button ID="Number_2_btn" runat="server" Text="2" OnClick="Number_2_btn_Click" />
+            <asp:Button ID="Number_3_btn" runat="server" Text="3" OnClick="Number_3_btn_Click" />
+            <asp:Button ID="Number_4_btn" runat="server" Text="4" OnClick="Number_4_btn_Click" />
+            <asp:Button ID="Number_5_btn" runat="server" Text="5" OnClick="Number_5_btn_Click" />
+            <asp:Button ID="Number_6_btn" runat="server" Text="6" OnClick="Number_6_btn_Click" />
+            <asp:Button ID="Number_7_btn" runat="server" Text="7" OnClick="Number_7_btn_Click" />
+            <asp:Button ID="Number_8_btn" runat="server" Text="8" OnClick="Number_8_btn_Click" />
+            <asp:Button ID="Number_9_btn" runat="server" Text="9" OnClick="Number_9_btn_Click" />
+
+            <asp:Button ID="Multiplication_btn" runat="server" Text="*" OnClick="Multiplication_btn_Click" />
+            <asp:Button ID="Division_btn" runat="server" Text="/" OnClick="Division_btn_Click" />
+            <asp:Button ID="Addition_btn" runat="server" Text="+" OnClick="Addition_btn_Click" />
+            <asp:Button ID="Subtraction_btn" runat="server" Text="-" OnClick="Subtraction_btn_Click" />
+
+            <asp:Button ID="Clear_btn" runat="server" Text="C" OnClick="Clear_btn_Click" />
+            <asp:Button ID="Result_btn" runat="server" Text="=" OnClick="Result_btn_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Calculator/Calculator.aspx.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calculator
+{
+    public partial class Calculator : System.Web.UI.Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+
+        protected void Number_0_btn_Click(object sender, EventArgs e)
+        {
+            tbx_expression.Text += 0.ToString();
+        }
+
+        protected void Number_1_btn_Click(object sender, EventArgs e)
+        {
+            tbx_expression.Text += 1.ToString();
+        }
+
+        protected void Number_2_btn_Click(object sender, EventArgs e)
+        {
+            tbx_expression.Text += 2.ToString();
+        }
+
+        protected void Number_3_btn_Click(object sender, EventArgs e)
+        {
+            tbx_expression.Text += 3.ToString();
+        }
+
+        protected void Number_4_btn_Click(object sender, EventArgs e)
+        {
+            tbx_expression.Text += 4.ToString();
+        }
+
+        protected void Number_5_btn_Click(object sender, EventArgs e)
+        {
+            tbx_expression.Text += 5.ToString();
+        }
+
+        protected void Number_6_btn_Click(object sender, EventArgs e)
+        {
+            tbx_expression.Text += 6.ToString();
+        }
+
+        protected void Number_7_btn_Click(object sender, EventArgs e)
+        {
+            tbx_expression.Text += 7.ToString();
+        }
+
+        protected void Number_8_btn_Click(object sender, EventArgs e)
+        {
+            tbx_expression.Text += 8.ToString();
+        }
+
+        protected void Number_9_btn_Click(object sender, EventArgs e)
+        {
+            tbx_expression.Text += 9.ToString();
+        }
+
+        protected void Multiplication_btn_Click(object sender, EventArgs e)
+        {
+            tbx_expression.Text += "*";
+        }
+
+        protected void Division_btn_Click(object sender, EventArgs e)
+        {
+            tbx_expression.Text += "/";
+        }
+
+        protected void Addition_btn_Click(object sender, EventArgs e)
+        {
+            tbx_expression.Text += "+";
+        }
+
+        protected void Subtraction_btn_Click(object sender, EventArgs e)
+        {
+            tbx_expression.Text += "-";
+        }
+
+        protected void Clear_btn_Click(object sender, EventArgs e)
+        {
+            tbx_expression.Text = "";
+        }
+
+        protected void Result_btn_Click(object sender, EventArgs e)
+        {
+            List<string> expression = new List<string>();
+            int it = 0;
+
+            for (int i = 0; i < tbx_expression.Text.Length; i++)
+            {
+                if ((int)tbx_expression.Text[i] > 47 && (int)tbx_expression.Text[i] < 58)
+                {
+                    expression.Add("");
+                    expression[it] += tbx_expression.Text[i];
+
+                }
+                else
+                {
+                    it++;
+                    expression[it] += tbx_expression.Text[i];
+                }
+            }
+            tbx_expression.Text = GetResult(expression);
+
+        }
+        private string GetResult(List<string> expression)
+        {
+            if (expression.Contains("*"))
+            {
+                int MIndex = expression.IndexOf("*");
+                string res = (Int32.Parse(expression[MIndex - 1]) * Int32.Parse(expression[MIndex + 1])).ToString();
+                expression.RemoveAt(MIndex - 1);
+                expression.RemoveAt(MIndex + 1);
+                expression[MIndex] = res;
+            }
+            if (expression.Contains("/"))
+            {
+                int DIndex = expression.IndexOf("/");
+                string res = (Int32.Parse(expression[DIndex - 1]) / Int32.Parse(expression[DIndex + 1])).ToString();
+                expression.RemoveAt(DIndex - 1);
+                expression.RemoveAt(DIndex + 1);
+                expression[DIndex] = res;
+            }
+            //if (!expression.Contains("*") && !expression.Contains("/"))
+            //{
+            int temp = 0;
+            for (int i = 0; i < expression.Count; i++)
+            {
+                if (Int32.TryParse(expression[i], out temp) == false)
+                {
+                    if (expression[i] == "+")
+                    {
+                        string res = (Convert.ToInt32(expression[i - 1]) + Convert.ToInt32(expression[i + 1])).ToString();
+                        expression.RemoveAt(i - 1);
+                        expression.RemoveAt(i + 1);
+                        expression[i] = res;
+                    }
+                    if (expression[i] == "-")
+                    {
+                        string res = (Convert.ToInt32(expression[i - 1]) - Convert.ToInt32(expression[i + 1])).ToString();
+                        expression.RemoveAt(i - 1);
+                        expression.RemoveAt(i + 1);
+                        expression[i] = res;
+                    }
+                }
+            }
+            //}
+            return expression[0];
+        }
+    }
+}
